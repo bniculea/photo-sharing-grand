ff4e174500e78b0c7b677642445b8581b1fe0353
bniculea <bogdan.niculeasa@hotmail.com>
2021-03-21 18:28:02 +0200

    Fix #3

--- server/dist/config/index.js
+++
@@ -1,4 +0,0 @@
-export const NEO4J_URI = 'http://localhost:7474';
-export const NEO4J_USER = 'neo4j';
-export const NEO4J_PASSWORD = 'admin';
-//# sourceMappingURL=index.js.map
--- server/dist/graphql/resolvers/index.js
+++
@@ -1,6 +0,0 @@
-import { UserResolver } from './users.js';
-const resolvers = [
-    UserResolver
-];
-export default resolvers;
-//# sourceMappingURL=index.js.map
